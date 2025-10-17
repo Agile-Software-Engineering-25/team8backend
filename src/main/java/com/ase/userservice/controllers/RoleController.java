d0fdf9bb32380b50a7ef74e1dffa04dc4329e52c
Simon <simmmfimmm@gmail.com>
2025-10-17 16:44:31 +0200

    Implement RoleController for role management

---
+++ src/main/java/com/ase/userservice/controllers/RoleController.java
@@ -0,0 +1,61 @@
+package com.example.keycloak.backend.controller;
+
+import com.example.keycloak.backend.dto.*;
+import com.example.keycloak.backend.service.KeycloakRoleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class RoleController {
+
+    private final KeycloakRoleService roleService;
+
+    public RoleController(KeycloakRoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<RoleDto>> getAllRoles() {
+        return ResponseEntity.ok(roleService.findAllRoles());
+    }
+
+    @GetMapping("/roles/{roleId}")
+    public ResponseEntity<RoleDetailDto> getRoleById(@PathVariable String roleId) {
+        return ResponseEntity.ok(roleService.findRoleById(roleId));
+    }
+    
+    @PostMapping("/roles")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createRole(@RequestBody CreateRoleRequest request) {
+        roleService.createRole(request);
+    }
+
+    @PutMapping("/roles/{roleId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateRole(@PathVariable String roleId, @RequestBody UpdateRoleRequest request) {
+        roleService.updateRole(roleId, request);
+    }
+
+    @PostMapping("/roles/{roleId}/permissions")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addPermission(@PathVariable String roleId, @RequestBody Map<String, String> payload) {
+        String permissionId = payload.get("permissionId");
+        roleService.addPermissionToRole(roleId, permissionId);
+    }
+
+    @DeleteMapping("/roles/{roleId}/permissions/{permissionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removePermission(@PathVariable String roleId, @PathVariable String permissionId) {
+        roleService.removePermissionFromRole(roleId, permissionId);
+    }
+    
+    @GetMapping("/standard-roles")
+    public ResponseEntity<List<String>> getStandardRoles() {
+        return ResponseEntity.ok(roleService.getStandardRoles());
+    }
+}
